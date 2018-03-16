ed696d01c6610b02298792de8873542ef8bf92b9
benoitgaujac <benoit.gaujac@gmail.com>
2018-03-16 10:20:18 +0000

    discrete colorbar

--- wae.py
+++ wae.py
@@ -840,7 +840,7 @@ def save_plots(opts, sample_train, label_train,
     ymax = ymax + width * magnify
     plt.xlim(xmin, xmax)
     plt.ylim(ymin, ymax)
-    plt.colorbar()
+    plt.colorbar(ticks = range(10))
 
     ###The loss curves
     ax = plt.subplot(gs[1, 2])
