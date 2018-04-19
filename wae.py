e1f1c5154bb7889c76d134a48b8d859f1284bcca
benoitgaujac <benoit.gaujac@gmail.com>
2018-04-19 10:54:59 +0100

    added opt sqrt_MMD

--- wae.py
+++ wae.py
@@ -297,8 +297,10 @@ class WAE(object):
                                         + opts['rg_lambda'] * one_sided_err \
                                         - opts['ae_lambda'] * MMD_regu
         else:
-            #MMD_penalty = tf.sqrt(MMD+1e-10)
-            MMD_penalty = MMD
+            if opts['sqrt_MMD']
+                MMD_penalty = tf.sqrt(MMD+1e-8)
+            else:
+                MMD_penalty = MMD
             self.mmd_objective = None
 
         return MMD_penalty
