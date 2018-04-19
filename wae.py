5eb1d5507558c8385f2493f6a6d2a9785c9aed26
benoitgaujac <benoit.gaujac@gmail.com>
2018-04-19 10:46:42 +0100

    coef recons

--- wae.py
+++ wae.py
@@ -400,7 +400,6 @@ class WAE(object):
         log_qz = tf.reduce_mean(log_qz,axis=0)
         kl_qz = tf.reduce_mean(log_qz)
 
-        #return kl_pz - kl_qz
         return tf.reduce_mean(log_pz-log_qz)
 
     def pretrain_loss(self):
@@ -509,7 +508,7 @@ class WAE(object):
 
     def pretrain_encoder(self, data):
         opts = self.opts
-        steps_max = 2000
+        steps_max = 1500
         batch_size = opts['e_pretrain_sample_size']
         for step in range(steps_max):
             train_size = data.num_points
