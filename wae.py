8c56cd4dfca6631b30de3656c4b2a21f0c4adf1e
benoitgaujac <benoit.gaujac@gmail.com>
2018-05-15 20:02:45 +0100

    training VAE

--- wae.py
+++ wae.py
@@ -785,7 +785,7 @@ class WAE(object):
                     # cond1_bis = counter < 101 and counter % 10==0
                     # cond1_bisbis = counter > 100 and counter % 100==0
                     # cond1 = cond1_bis or cond1_bisbis
-                    cond1 = ccounter < 101 and counter % 10==0
+                    cond1 = counter < 101 and counter % 10==0
                 else:
                     cond1 = counter==1
                 cond2 = counter % opts['print_every'] == 0
