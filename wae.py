230949f00d1e64308182f15adc5ea218e1afa6e6
benoitgaujac <benoit.gaujac@gmail.com>
2018-05-17 10:15:28 +0100

    training VAE from SWAE

--- wae.py
+++ wae.py
@@ -1286,8 +1286,8 @@ def save_plots(opts, sample_train,sample_test,
                                 metric='correlation').fit_transform(np.concatenate((encoded,sample_prior),axis=0))
 
     plt.scatter(embedding[:num_pics, 0], embedding[:num_pics, 1],
-                #c=label_test[:num_pics], s=40, label='Qz test',cmap=discrete_cmap(10, base_cmap='tab10'))
-                c=label_test[:num_pics], s=40, label='Qz test',cmap=discrete_cmap(10, base_cmap='Vega10'))
+                c=label_test[:num_pics], s=40, label='Qz test',cmap=discrete_cmap(10, base_cmap='tab10'))
+                #c=label_test[:num_pics], s=40, label='Qz test',cmap=discrete_cmap(10, base_cmap='Vega10'))
     plt.colorbar()
     plt.scatter(embedding[num_pics:, 0], embedding[num_pics:, 1],
                             color='navy', s=10, marker='*',label='Pz')
