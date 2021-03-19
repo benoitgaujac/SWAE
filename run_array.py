c2d91fc74d5edcd472695e20a9c98dd1183b7201
benoitgaujac <benoit.gaujac@gmail.com>
2021-03-19 19:39:34 +0000

    wae & vae

--- run_array.py
+++ run_array.py
@@ -84,7 +84,7 @@ def main():
         opts['zdim'] = FLAGS.zdim
     opts['full_cov_matrix'] = FLAGS.full_cov_mat
     opts['cost'] = FLAGS.cost
-    betas = [1,10,100,200,350,500,650,800,1000,1,5,10]
+    betas = [1,10,100,200,350,500,650,800,1000,1,2,10]
     # betas = [1,5,10]
     coef_id = (FLAGS.id-1) % len(betas)
     opts['beta'] = betas[coef_id]
